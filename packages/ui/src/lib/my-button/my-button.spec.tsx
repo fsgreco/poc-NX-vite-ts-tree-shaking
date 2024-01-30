3cb7ddf34bec2653d5baf2e1b665614ac7790499
Santiago <fsgreco@fastmail.com>
2024-01-30 19:43:02 +0100

    Improve testing

--- packages/ui/src/lib/my-button/my-button.spec.tsx
+++ packages/ui/src/lib/my-button/my-button.spec.tsx
@@ -1,10 +1,24 @@
-import { render } from '@testing-library/react';
-
+import React from 'react';
 import MyButton from './my-button';
 
-describe('MyButton', () => {
+import { render, screen } from '@testing-library/react';
+import { describe, it, test, expect } from 'vitest';
+
+// @vitest-environment jsdom
+
+describe('MyButton Component', () => {
+
+	let buttonContent = 'ciaone'
+	const { baseElement } = render(<MyButton label={buttonContent} />);
+
+	// screen.debug(document.body);
+
   it('should render successfully', () => {
-    const { baseElement } = render(<MyButton />);
     expect(baseElement).toBeTruthy();
   });
+
+	test('it contains the correct label', () => {
+		let text = document.querySelector('button')?.textContent 
+		expect(text).toBe(buttonContent)
+	})
 });
